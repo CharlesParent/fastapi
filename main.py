7d8e86b11a1679cd1e8687d98a6a1b0dbe22e106
Charles Parent <cha.parent@gmail.com>
2023-04-03 09:11:41 +0200

    Body parameters in POST queries

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
+    description: str | None = None
     price: float
-    is_offer: Union[bool, None] = None
+    tax: float | None = None
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 @app.get("/")
@@ -17,11 +21,25 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item_by_id(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
